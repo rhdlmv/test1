9cacecfb07940254b4bd6e36abb395aa288830c0
rhdlmv <rhdlmv@126.com>
2015-08-31 18:32:59 +0800

    Nothing

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,55 +1,56 @@
 ﻿using System;
-              using System.Collections.Generic;
-              using System.Linq;
-              using StockTools.Utils; 
-              using StockScript;
-              using DataObject;
+using System.Collections.Generic;
+using System.Linq;
+using StockTools.Utils;
+using StockScript;
+using DataObject;
 
 
-              namespace Test.Indicator
-              {
-                    public class paul_-testddd : IIndicator
-                    {
-
-                        public paul_-testddd()
-                        {
-                            
-                        }   
-
-                        private string _alias = "paul1";
-
-
-                        public string Name { get { return "paul_-testddd";} }
-public string Alias
+namespace Test.Indicator
 {
-    get { return _alias; }
-    set { _alias = value;  }
+    public class paultest2 : IIndicator
+    {
+
+        public paultest2()
+        {
+
+        }
+
+        private string _alias = "paulTest2";
+
+
+        public string Name { get { return "paultest2"; } }
+        public string Alias
+        {
+            get { return _alias; }
+            set { _alias = value; }
+        }
+
+
+        public double dd(CSTradeScriptContext context, Application application, HistoryPrices historyPrices)
+        {
+            return double.NaN;
+        }
+
+        public int GetParameterCount() { return 1; }
+        public Type[] GetParameterTypes() { return new Type[] { typeof(Int32) }; }
+        public Dictionary<string, List<Data>> GetIndicatorDatas(CSTradeScriptContext context, object[] parameters)
+        {
+            Dictionary<string, List<Data>> results = new Dictionary<string, List<Data>>();
+            context.LogEntityType = LogEntityType.INDICATOR;
+            results.Add("dd", new List<Data>());
+            context.Arguments.gg = ConvertUtils.ToObject<int>(parameters[0]);
+            context.CalculatingIndicatorName = this.Alias;
+            foreach (DateTime date in context.GetProcessDates())
+            {
+                context.CurrentDate = date;
+                double dd = dd(context, context.GetDataByDate<Application>(date), context.GetDataByDate<HistoryPrices>(date));
+                context.SetIndicatorData(this.Alias, "dd", dd);
+                results["dd"].Add(new Data() { Date = date, Value = dd });
+            }
+            return results;
+        }
+
+
+    }
 }
-
-
-                        public double ddd(CSTradeScriptContext context, HistoryPrices historyPrices)
-{
-	return double.NaN;
-}
-
-public int GetParameterCount() { return 0; }
-public Type[] GetParameterTypes() { return null; }
-public Dictionary<string, List<Data>> GetIndicatorDatas(CSTradeScriptContext context, object[] parameters)
-{
-Dictionary<string, List<Data>> results = new Dictionary<string, List<Data>>();
-context.LogEntityType = LogEntityType.INDICATOR;
-results.Add("ddd", new List<Data>());
-context.CalculatingIndicatorName = this.Alias;
-foreach(DateTime date in context.GetProcessDates())
-{
-context.CurrentDate = date;
-double ddd = ddd(context, context.GetDataByDate<HistoryPrices>(date));
-context.SetIndicatorData(this.Alias, "ddd", ddd);
-results["ddd"].Add(new Data() { Date = date, Value = ddd });
-}
-return results;
-}
-
-
-                    }
-              }
