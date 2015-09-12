54559a3d35fc4a9be66146a0c5fb39f8696b5199
rhdlmv <rhdlmv@126.com>
2015-09-12 10:51:19 +0800

    add console app  func_test

---
+++ Func_Test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Func_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetTByKey("member", GetMemberCount));
+            Console.WriteLine(GetTByKey("account", GetAccountName));
+            Console.ReadKey();
+        }
+
+        static T GetTByKey<T>(string key, Func<string, bool, T> get)
+        {
+            //other
+            var condition = string.Format("key = {0}", key);
+            Console.WriteLine(condition);
+            var result = get(condition, false);
+            return result;
+        }
+
+        static int GetMemberCount(string condition, bool isExit)
+        {
+            return 10;
+        }
+
+        static string GetAccountName(string condition, bool isExit)
+        {
+            return "hellale";
+        }
+
+
+
+
+    }
+}
--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,56 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using StockTools.Utils;
-using StockScript;
-using DataObject;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using StockTools.Utils;
+//using StockScript;
+//using DataObject;
 
 
-namespace Test.Indicator
-{
-    public class paultest2 : IIndicator
-    {
+//namespace Test.Indicator
+//{
+//    public class paultest2 : IIndicator
+//    {
 
-        public paultest2()
-        {
+//        public paultest2()
+//        {
 
-        }
+//        }
 
-        private string _alias = "paulTest2";
-
-
-        public string Name { get { return "paultest2"; } }
-        public string Alias
-        {
-            get { return _alias; }
-            set { _alias = value; }
-        }
+//        private string _alias = "paulTest2";
 
 
-        public double dd(CSTradeScriptContext context, Application application, HistoryPrices historyPrices)
-        {
-            return double.NaN;
-        }
-
-        public int GetParameterCount() { return 1; }
-        public Type[] GetParameterTypes() { return new Type[] { typeof(Int32) }; }
-        public Dictionary<string, List<Data>> GetIndicatorDatas(CSTradeScriptContext context, object[] parameters)
-        {
-            Dictionary<string, List<Data>> results = new Dictionary<string, List<Data>>();
-            context.LogEntityType = LogEntityType.INDICATOR;
-            results.Add("dd", new List<Data>());
-            context.Arguments.gg = ConvertUtils.ToObject<int>(parameters[0]);
-            context.CalculatingIndicatorName = this.Alias;
-            foreach (DateTime date in context.GetProcessDates())
-            {
-                context.CurrentDate = date;
-                double dd = dd(context, context.GetDataByDate<Application>(date), context.GetDataByDate<HistoryPrices>(date));
-                context.SetIndicatorData(this.Alias, "dd", dd);
-                results["dd"].Add(new Data() { Date = date, Value = dd });
-            }
-            return results;
-        }
+//        public string Name { get { return "paultest2"; } }
+//        public string Alias
+//        {
+//            get { return _alias; }
+//            set { _alias = value; }
+//        }
 
 
-    }
-}
+//        public double dd(CSTradeScriptContext context, Application application, HistoryPrices historyPrices)
+//        {
+//            return double.NaN;
+//        }
+
+//        public int GetParameterCount() { return 1; }
+//        public Type[] GetParameterTypes() { return new Type[] { typeof(Int32) }; }
+//        public Dictionary<string, List<Data>> GetIndicatorDatas(CSTradeScriptContext context, object[] parameters)
+//        {
+//            Dictionary<string, List<Data>> results = new Dictionary<string, List<Data>>();
+//            context.LogEntityType = LogEntityType.INDICATOR;
+//            results.Add("dd", new List<Data>());
+//            context.Arguments.gg = ConvertUtils.ToObject<int>(parameters[0]);
+//            context.CalculatingIndicatorName = this.Alias;
+//            foreach (DateTime date in context.GetProcessDates())
+//            {
+//                context.CurrentDate = date;
+//                double dd = dd(context, context.GetDataByDate<Application>(date), context.GetDataByDate<HistoryPrices>(date));
+//                context.SetIndicatorData(this.Alias, "dd", dd);
+//                results["dd"].Add(new Data() { Date = date, Value = dd });
+//            }
+//            return results;
+//        }
+
+
+//    }
+//}
