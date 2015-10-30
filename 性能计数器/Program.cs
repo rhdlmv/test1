55a1e914f82c2ae7bf013fe99e528908153ff11b
Jimi <rhdlmv@126.com>
2015-10-31 01:00:47 +0800

    nothing

--- 性能计数器/Program.cs
+++ 性能计数器/Program.cs
@@ -14,7 +14,7 @@ namespace 性能计数器
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault(false); 
             Application.Run(new Form1());
         }
     }
